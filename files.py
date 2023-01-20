b6ccf854acf5a1a4aa8d9643c52e02ba3d33bae4
winstr <kimtmdgus22@gmail.com>
2023-01-20 05:28:22 +0000

    Modified some variables and comment

--- files.py
+++ files.py
@@ -2,21 +2,22 @@ import os
 import fnmatch
 
 
-def get_files(root:str, regex:str, recursive:bool):
-    """
+def get_files(root_dir:str, posix_regex:str, recursive:bool=True):
+    """ Get the file paths that match the posix regular expression
+        from root directory.
     params:
-        root(str): root dir path.
-        regex(str): linux filename pattern - posix regular expression.
-        recursive(bool): if true, search files recursively.
+        root_dir(str): the root directory path
+        posix_regex(str): posix regular expression (e.g. "*.mp4")
+        recursive(bool): if true, search files recursively
     returns:
-        generator
+        Generator[str, None, None]: yield a file path
     """
-    if not os.path.exists(root):
-        msg = f"{root} is not exists or not a directory."
-        raise ValueError(msg)
-    for par_dir, _, files in os.walk(root):
-        if files:
-            for file in fnmatch.filter(files, regex):
-                yield os.path.join(par_dir, file)
+    if not os.path.exists(root_dir):
+        msg = f"{root_dir} is not exists or not a directory."
+        raise FileNotFoundError(msg)
+    for parent_dir, _, files in os.walk(root_dir):
+        if files:  # if files are exist
+            for file_ in fnmatch.filter(files, posix_regex):
+                yield os.path.join(parent_dir, file_)
         if not recursive:
             break
